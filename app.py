a39a11078fb4ba33e22156697c207f67ae1ecc9b
manucitavm-web <manucitavm@gmail.com>
2026-04-09 13:40:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain.chains.question_answering import load_qa_chain
 import platform
 
 # App title and presentation
-st.title('Generación Aumentada por Recuperación (RAG) 💬')
+st.title('Voluntariado: Ayudar transforma el mundo. 💬')
 st.write("Versión de Python:", platform.python_version())
 
 # Load and display image
